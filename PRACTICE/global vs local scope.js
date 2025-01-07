8b6081fcf4a7359863f2f7f16ffb8795eaa497c9
Jay Dhobi <138579588+jay3297@users.noreply.github.com>
2025-01-07 23:20:11 +0530

    global vs local scope

---
+++ PRACTICE/global vs local scope.js
@@ -0,0 +1,56 @@
+// // const username ="jay";
+// // const number ="22";
+// // function func() {
+// //     const x = 5;
+// //     const y = 6;
+// //     console.log(x+y);
+// // }
+// // func();
+
+// // var username ='jay';
+// // function func()
+// // {
+// //     console.log(username);
+// // }
+// // func()
+
+// function func() {
+//     let a=10;
+//     let b=20;
+//     console.log(a+b);
+// }
+// func();
+// c = 10
+
+// function func()
+// {
+//     let username ="jay";
+//     function child()
+//     {
+//         const pi=3.15;
+//         console.log(username);
+//     }
+//     child()
+//     // console.log(username);
+//     console.log( typeof c)
+// }
+// func()
+
+
+{ 
+    var ea = 9;;
+    {
+        console.log(xc + "hh");
+        let ec = 90;
+        {
+            var xc = 90;
+            var cx = 99;
+            console.log(xc);
+            console.log(xc);
+        }
+        console.log(xc);
+    }
+    console.log(ea);
+}
+
+console.log(ea);
---
+++ PRACTICE/variable types/script.js
@@ -0,0 +1,11 @@
+let username = 'jay';
+console.log(typeof username);
+let password =1234;
+console.log(typeof password)
+let ishappy = true;
+console.log(ishappy);
+console.log('hi i am '+ username);
+let age = 21;
+console.log('hi i am '+ age +' '+ 'years old.');
+username ='kuldeep';
+console.log(username);
